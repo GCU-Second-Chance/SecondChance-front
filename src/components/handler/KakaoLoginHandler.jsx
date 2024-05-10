69ad4b037967eb55ca8c6cf4d6a28271f3cba859
jiminseong <iamjms4237@gmail.com>
2024-05-10 20:12:28 +0900

    KakaoLoginHandler 수정

--- src/components/handler/KakaoLoginHandler.jsx
+++ src/components/handler/KakaoLoginHandler.jsx
@@ -11,7 +11,7 @@ const KakaoLoginHandler = () => {
     const code = new URL(window.location.href).searchParams.get("code");
 
     const kakaoLogin = () => {
-        axios({url:`${import.meta.env.VITE_SERVER_URL}/v1/login/kakao-login` , method:'post', data: { code : code} })
+        axios({url:`https://cors-anywhere.herokuapp.com${import.meta.env.VITE_SERVER_URL}/v1/login/kakao-login` , method:'post', data: { code : code} })
             .then(response => {
             const { id, username } = response.data;
             setCookie('userId', id, { path: '/' });
