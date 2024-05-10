e4e9145ccafa6482ca2eb31551064722ffdb7315
jiminseong <iamjms4237@gmail.com>
2024-05-10 23:55:04 +0900

    post -> get

--- src/components/handler/KakaoLoginHandler.jsx
+++ src/components/handler/KakaoLoginHandler.jsx
@@ -4,32 +4,31 @@ import { useCookies } from 'react-cookie';
 import axios from "axios";
 import Loading from "../layout/Loading";
 
-
 const KakaoLoginHandler = () => {
     const [cookies, setCookie] = useCookies(['userId', 'userName']);
     const navigate = useNavigate();
     const code = new URL(window.location.href).searchParams.get("code");
 
     const kakaoLogin = () => {
-        axios({url:`https://port-0-secondchance-back-test-1cupyg2klvnmgdft.sel5.cloudtype.app/v1/login/kakao-login` , method:'post', data: { code : code} })
+        axios.get(`${import.meta.env.VITE_SERVER_URL}/v1/login/kakao-login?code=${code}`)
             .then(response => {
-            const { id, username } = response.data;
-            setCookie('userId', id, { path: '/' });
-            setCookie('userName', username, { path: '/' });
-            navigate('/home');
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+                const { id, username } = response.data;
+                setCookie('userId', id, { path: '/' });
+                setCookie('userName', username, { path: '/' });
+                navigate('/home');
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
     }
-    
+
     useEffect(() => {
         kakaoLogin();
     }, []);
 
     return (
         <>
-            <Loading/>
+            <Loading />
         </>
     );
 };
