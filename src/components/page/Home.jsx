7ad164865cf5d3b3177d5483f4ca01941f90c270
jiminseong <iamjms4237@gmail.com>
2024-05-10 16:12:01 +0900

    서버 주소 수정

--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
 
     const donate = async (userId) => {
         try {
-            const res = await axios.post(`https://port-0-secondchance-back-1cupyg2klvnmgdft.sel5.cloudtype.app/v1/feature/donation`, { id: userId });
+            const res = await axios.post(`${import.meta.env.VITE_SERVER_URL}}v1/feature/donation`, { id: userId });
             console.log('Donation response:', res.data);
         } catch (error) {
             console.error('Error donating:', error);
