2afb958b9b59e52809f85b6c57005c7276dfa879
jiminseong <iamjms4237@gmail.com>
2024-05-10 18:39:56 +0900

    v1/login/kakao-login -> /kakao-login

--- src/components/layout/LayOut.jsx
+++ src/components/layout/LayOut.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
-function LayOut(props) {
+function LayOut() {
     return (
         <Back> 
             <Outlet/>
