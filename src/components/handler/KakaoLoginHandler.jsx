31b2783bc7af42d302147a7322ea76b87f86aecb
jiminseong <iamjms4237@gmail.com>
2024-05-13 14:00:17 +0900

    KakaoLoginHandler 수정

--- src/components/handler/KakaoLoginHandler.jsx
+++ src/components/handler/KakaoLoginHandler.jsx
@@ -1,6 +1,6 @@
-import { useNavigate } from "react-router-dom";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useCookies } from 'react-cookie';
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Loading from "../layout/Loading";
 
@@ -8,27 +8,34 @@ const KakaoLoginHandler = () => {
     const [cookies, setCookie] = useCookies(['userId', 'userName']);
     const navigate = useNavigate();
     const code = new URL(window.location.href).searchParams.get("code");
+    const [loading, setLoading] = useState(true); // 로딩 상태 추가
 
-    const kakaoLogin = () => {
-        axios.get(`${import.meta.env.VITE_SERVER_URL}/v1/login/kakao-login?code=${code}`)
-            .then(response => {
+    useEffect(() => {
+        const kakaoLogin = async () => { // 비동기 함수로 변경
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/v1/login/kakao-login?code=${code}`);
                 const { id, username } = response.data;
                 setCookie('userId', id, { path: '/' });
                 setCookie('userName', username, { path: '/' });
                 navigate('/home');
-            })
-            .catch(error => {
+            } catch (error) {
                 console.error('Error fetching data:', error);
-            });
-    }
+                // 여기에 사용자에게 보여줄 에러 처리 추가 가능
+            } finally {
+                setLoading(false); // 로딩 상태 갱신
+            }
+        };
 
-    useEffect(() => {
-        kakaoLogin();
-    }, []);
+        if (code) { // code가 존재할 때만 요청
+            kakaoLogin();
+        } else {
+            setLoading(false); // code가 없는 경우에도 로딩 상태 갱신
+        }
+    }, [code, navigate, setCookie]); // 의존성 목록에 추가
 
     return (
         <>
-            <Loading />
+            {loading ? <Loading /> : null} {/* 로딩 상태에 따라 Loading 컴포넌트 표시 */}
         </>
     );
 };
