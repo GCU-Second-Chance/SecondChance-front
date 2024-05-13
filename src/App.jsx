5822c7eedb79c9c4a70ececf5e3064f9f6e699ba
jiminseong <iamjms4237@gmail.com>
2024-05-13 14:39:28 +0900

    오타 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import './App.css'
 import Login from './components/page/Login'
 import Home from './components/page/Home'
 import Outro from './components/page/TakeAway'
-import KakaoLoginHandeler from './components/handler/KakaoLoginHandler'
+import KakaoLoginHandler from './components/handler/KakaoLoginHandler'
 import { Routes, Route } from 'react-router-dom'; 
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/kakao/login" element={<KakaoLoginHandeler/>} />
+      <Route path="/kakao/login" element={<KakaoLoginHandler/>} />
       <Route path="/home" element={<Home/>} />
       <Route path="/outro" element={<Outro/>} />
       <Route exact path="/" element={<Login/>} />
