3ba16cd6ef8bf1905f4d45486355cd3b34f22864
jiminseong <iamjms4237@gmail.com>
2024-05-13 13:26:22 +0900

    <Route path=/kakao/login/:code component={KaKaoLoginHandeler} />

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Login from './components/page/Login'
 import Home from './components/page/Home'
 import Outro from './components/page/TakeAway'
 import KaKaoLoginHandeler from './components/handler/KakaoLoginHandler'
+import DogDetail from './components/dog/dogdetail/DogDetail'
 import { Switch, Route } from 'react-router-dom'; 
 
 function App() {
   return (
     <>
     <Switch>
-      <Route path="/kakao/login?code=:code" component={KaKaoLoginHandeler} />
+      <Route path="/kakao/login/:code" component={KaKaoLoginHandeler} />
+      <Route path="/kakao/login" component={KaKaoLoginHandeler} />
       <Route path="/home" component={Home} />
       <Route path="/outro" component={Outro} />
       <Route exact path="/" component={Login} />
