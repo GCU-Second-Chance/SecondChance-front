919390e052f9933ab88b388a6060f8edf18dc6e3
jiminseong <iamjms4237@gmail.com>
2024-05-13 12:35:00 +0900

    App.jsx , main.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,22 @@ import Login from './components/page/Login'
 import Home from './components/page/Home'
 import Outro from './components/page/TakeAway'
 import KaKaoLoginHandeler from './components/handler/KakaoLoginHandler'
-import { Switch, Route } from 'react-router-dom'
 import DogDetail from './components/dog/dogdetail/DogDetail'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'; 
 
 function App() {
   return (
-    <Switch>
-      <Route path="/kakao/login" component={KaKaoLoginHandeler} />
-      <Route>
-        <LayOut>
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route path="/home" component={Home} />
-            <Route path="/outro" component={Outro} />
-            <Route path="/home/:id" component={DogDetail} />
-          </Switch>
-        </LayOut>
-      </Route>
-    </Switch>
+    <Router> {/* Router로 감싸기 */}
+      <LayOut>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/kakao/login" component={KaKaoLoginHandeler} />
+          <Route path="/home" component={Home} />
+          <Route path="/outro" component={Outro} />
+          <Route path="/home/:id" component={DogDetail} />
+        </Switch>
+      </LayOut>
+    </Router>
   )
 }
 
