c7e240d19bc0a9e43bcff0cfa602a97295b5983b
jiminseong <iamjms4237@gmail.com>
2024-05-13 13:03:02 +0900

    LayOut.jsx delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
 import './App.css'
-import LayOut from './components/layout/LayOut'
+
 import Login from './components/page/Login'
 import Home from './components/page/Home'
 import Outro from './components/page/TakeAway'
 import KaKaoLoginHandeler from './components/handler/KakaoLoginHandler'
 import DogDetail from './components/dog/dogdetail/DogDetail'
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'; 
+import { Switch, Route } from 'react-router-dom'; 
 
 function App() {
   return (
-    <Router> {/* Router로 감싸기 */}
-      <LayOut>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/kakao/login" component={KaKaoLoginHandeler} />
-          <Route path="/home" component={Home} />
-          <Route path="/outro" component={Outro} />
-          <Route path="/home/:id" component={DogDetail} />
-        </Switch>
-      </LayOut>
-    </Router>
+    <Switch>
+      <Route path="/kakao/login" component={KaKaoLoginHandeler} />
+      <Route path="/home" component={Home} />
+      <Route path="/outro" component={Outro} />
+      <Route path="/home/:id" component={DogDetail} />
+      <Route path="/" component={Login} />
+  </Switch>
   )
 }
 
--- src/components/layout/LayOut.jsx
+++ src/components/layout/LayOut.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
+// import React from 'react';
+// import styled from 'styled-components';
 
-function LayOut({ children }) {
-    return (
-        <Back> 
-            {children}
-        </Back>
-    );
-}
+// function LayOut({ children }) {
+//     return (
+//         <Back> 
+//             {children}
+//         </Back>
+//     );
+// }
 
-export default LayOut;
+// export default LayOut;
 
 
-const Back = styled.div`
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-    display: flex;
-    flex-direction: column;
+// const Back = styled.div`
+//     width: 100%;
+//     height: 100%;
+//     background-color: #fff;
+//     display: flex;
+//     flex-direction: column;
 
-`;
+// `;
