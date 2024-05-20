27a3822ba71cc53b0251e8af13a05f1e7509a141
jiminseong <iamjms4237@gmail.com>
2024-05-20 19:24:55 +0900

    home 수정2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import Home from './components/page/Home'
 import { Routes, Route } from 'react-router-dom'; 
 import Onboarding from './components/page/Onboarding';
+import KakaoShare from './components/handler/KakaoShare';
 
 function App() {
   return (
     <>
     <Routes>
+      <Route path='/share' element={<KakaoShare/>}/>
       <Route path="/home" element={<Home/>} />
       <Route exact path="/" element={<Onboarding/>} />
     </Routes>
